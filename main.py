613579bdf6a0c190223bd330dda1d2862ea1fb74
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2024-05-24 20:20:39 +0300

    Fixed some calls to unexisting functions.

--- main.py
+++ main.py
@@ -15,15 +15,12 @@ BATCH_SIZE = 32
 if __name__ == "__main__":
     # TODO: take the images, use opencv to convert to hsl, and create the tensoflow datasets
 
+    train_ds = keras.utils.image_dataset_from_directory(TRAIN_DATA_PATH,
+                                                        shuffle=True,
+                                                        batch_size=BATCH_SIZE,
+                                                        image_size=IMG_SIZE)
 
-    train_images = read_all_images(TRAIN_DATA_PATH)
-    validation_iamges = read_all_images(VALIDATION_DATA_PATH)
-    train_ds = keras.utils.image_dataset_from_tensor_slices(train_images,
-                                                         shuffle = True,
-                                                         batch_size = BATCH_SIZE,
-                                                         image_size = IMG_SIZE)
-
-    validation_ds = keras.utils.image_dataset_from_tensor_slices(validation_iamges,
-                                                               shuffle=True,
-                                                               batch_size=BATCH_SIZE,
-                                                               image_size=IMG_SIZE)
+    validation_ds = keras.utils.image_dataset_from_directory(VALIDATION_DATA_PATH,
+                                                             shuffle=True,
+                                                             batch_size=BATCH_SIZE,
+                                                             image_size=IMG_SIZE)
