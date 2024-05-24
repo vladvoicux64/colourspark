c39e6d6de60f8e4b59483e6d17488967cd4719be
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2024-05-24 22:55:40 +0300

    Designed a model and refactored data a  bit.

--- data_utils.py
+++ data_utils.py
@@ -1,8 +1,9 @@
 import cv2
+import numpy as np
 
 
-def convert_to_hls(images):
-    conversion = lambda image: cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+def convert_to_grayscale(images):
+    output = []
     for image in images:
-        conversion(image)
-    return images
+        output.append(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY))
+    return np.array(output).reshape((len(images), 96, 96, 1))
--- main.py
+++ main.py
@@ -1,33 +1,67 @@
 import keras
-from stl10_input import read_all_images
-from data_utils import convert_to_hls
-import numpy
 import matplotlib.pyplot as plt
-import tensorflow as tf
-
-
-# because this model will only need unsupervised training
-# we will treat the original training set as a validation set
-# and use only the unlabeled set for training
+import data_utils as du
+from keras.src.callbacks import TensorBoard
+from stl10_input import read_all_images
 
 TRAIN_DATA_PATH = './data/stl10_binary/unlabeled_X.bin'
 VALIDATION_DATA_PATH = './data/stl10_binary/train_X.bin'
 IMG_SIZE = (96, 96)
 BATCH_SIZE = 32
 if __name__ == "__main__":
-    # TODO: take the images, use opencv to convert to hsl, and create the tensoflow datasets
-
     # read images from disk
     train_images = read_all_images(TRAIN_DATA_PATH)
 
     validation_images = read_all_images(VALIDATION_DATA_PATH)
 
-    # convert to hsl
-    train_images = convert_to_hls(train_images)
-
-    validation_images = convert_to_hls(validation_images)
-
     # create datasets
-    train_ds = tf.data.Dataset.from_tensor_slices(train_images)
+    train_ds_x = du.convert_to_grayscale(train_images).astype('float32') / 255
 
-    validation_ds = tf.data.Dataset.from_tensor_slices(validation_images)
+    train_ds_y = train_images.astype('float32') / 255
+
+    validation_ds_x = du.convert_to_grayscale(validation_images).astype('float32') / 255
+
+    validation_ds_y = validation_images.astype('float32') / 255
+
+    # model
+    input_img = keras.Input(IMG_SIZE + (1,))
+
+    x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
+    x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
+    x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+    encoded = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
+
+    x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
+    x = keras.layers.UpSampling2D((2, 2))(x)
+    x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+    x = keras.layers.UpSampling2D((2, 2))(x)
+    decoded = keras.layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
+
+    autoencoder = keras.Model(input_img, decoded)
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+    autoencoder.fit(train_ds_x, train_ds_y,
+                    epochs=100,
+                    batch_size=BATCH_SIZE,
+                    shuffle=True,
+                    validation_data=(validation_ds_x, validation_ds_y),
+                    callbacks=[TensorBoard(log_dir='/tmp/tb')])
+
+    decoded_imgs = autoencoder.predict(validation_ds_x[:10])
+
+    n = 10
+    plt.figure(figsize=(20, 4))
+    for i in range(1, n + 1):
+        # Display original
+        ax = plt.subplot(2, n, i)
+        plt.imshow(validation_ds_x[i].reshape(96, 96))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # Display reconstruction
+        ax = plt.subplot(2, n, i + n)
+        plt.imshow(decoded_imgs[i].reshape(96, 96))
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
